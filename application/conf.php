5818a69c5c172224e0b3f069ad16ef0d3ad3aabe
Jose Albea <jose.albea@gmail.com>
2013-10-16 14:15:46 +0200

    Ajout du conf.php dans le gitignore

--- application/conf.php
+++
@@ -1,5 +0,0 @@
-<?php 
-define("SITE_ROOT", "http://localhost:8888/Fbapp/");
-define("SITE_NAME", "Qui veut gagner des Open ESGI");
-define("PUBLIC_ROOT", "http://localhost:8888/Fbapp/public/");
-?>
