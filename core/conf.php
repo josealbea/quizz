3c612f5157a256fdf283ac2c0a1c10945f174e51
Axel29 <axel.bouaziz@hotmail.fr>
2013-10-22 00:16:49 +0200

    Ajout fichier conf.php pour définitions variables globales
    Répertoire: ROOT/core

---
+++ core/conf.php
@@ -0,0 +1,13 @@
+<?php
+define('WEBROOT', str_replace('index.php', '', $_SERVER['SCRIPT_NAME']));
+define('SITE_NAME', 'Open ESGI Quizz');
+
+// Database access
+define('DB', 'mysql:dbname=facebook;host=localhost;charset=utf8');
+define('USER', 'root');
+define('PASSWORD', 'root');
+
+// Facebook configuration
+define('APPID', '194744407377894');
+define('APPSECRET', '18559abeb6be6a8c8a26a60bd645d1fb');
+define('PAGEID', '535494466533197');
--- index.php
+++ index.php
@@ -1,17 +1,5 @@
 <?php
-define('WEBROOT', str_replace('index.php', '', $_SERVER['SCRIPT_NAME']));
 define('ROOT', str_replace('index.php', '', $_SERVER['SCRIPT_FILENAME']));
-define('SITE_NAME', 'Open ESGI Quizz');
-
-// Database access
-define('DB', 'mysql:dbname=facebook;host=localhost;charset=utf8');
-define('USER', 'root');
-define('PASSWORD', 'root');
-
-// Facebook configuration
-define('APPID', '194744407377894');
-define('APPSECRET', '18559abeb6be6a8c8a26a60bd645d1fb');
-define('PAGEID', '535494466533197');
 
 require_once(ROOT.'core/model.php');
 require_once(ROOT.'core/controller.php');
