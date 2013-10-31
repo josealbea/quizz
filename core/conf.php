e80bec042407b1367bfc9b7d641d80c1cf618500
Axel29 <axel.bouaziz@hotmail.fr>
2013-10-31 19:32:09 +0100

    Test gitignore

--- core/conf.php
+++ core/conf.php
@@ -12,4 +12,4 @@ define('APPID', '194744407377894');
 define('APPSECRET', '18559abeb6be6a8c8a26a60bd645d1fb');
 define('PAGEID', '535494466533197');
 define('MAX_QUESTIONS', 10);
-define('QUESTION_PER_LEVEL',  5);
+define('QUESTION_PER_LEVEL', 5);
