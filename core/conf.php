94eb85abbc1ae6a03ca4d503e66d1068705bbe2b
Axel29 <axel.bouaziz@hotmail.fr>
2013-10-31 19:26:20 +0100

    Modification conf.php
    Ajout maximum de questions par level, redéfinition du nombre maximal de question.

--- core/conf.php
+++ core/conf.php
@@ -11,4 +11,5 @@ define('PASSWORD', 'root');
 define('APPID', '194744407377894');
 define('APPSECRET', '18559abeb6be6a8c8a26a60bd645d1fb');
 define('PAGEID', '535494466533197');
-define('MAX_QUESTIONS', 3);
+define('MAX_QUESTIONS', 10);
+define('QUESTION_PER_LEVEL', 5);
