002608d4c37f4ff6832db2902b06e9ebeb71b4cd
yohann-teisseire <accountuser@MacBook-Pro-de-account.local>
2013-11-18 19:45:43 +0100

    bouttons

--- view/index/question.php
+++ view/index/question.php
@@ -75,8 +75,8 @@ jQuery(document).ready(function() {
 <div id="question">
 	<div id="derniermot">
 		<img src="<?php echo $quizz->getImageUrl('derniermot.png'); ?>" alt="dernier mot" />
-		<div class="a_demo_two">OUI</div>
-		<div class="a_demo_two" >NON</div>
+		<div class="button a_demo_two">OUI</div>
+		<div class="button a_demo_two" >NON</div>
 	</div>
 	<div class="shareResult">
 		<a href="#" id="share-result-link">Partager le résultat</a>';
